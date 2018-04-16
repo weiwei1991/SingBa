d3215e963011aa87c4175d709a13f31e1ea48c8b
董维 <1121721083@qq.com>
2018-04-16 18:13:46 +0800

    ok

--- CBAPIManager.swift
+++ CBAPIManager.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+
+///// 还可以
 /**"http://vapi.changba.com/ktvbox_apiproxy.php?ac=gethottestwork&curuserid=71506577&channelsrc=appstore&version=7.1&token=T1204e26ad51dd4a&bless=0&macaddress=1DEB5999-B8C0-41F3-BF04-A3F622C49715&ismember=0&openudid=a6ad5ed82d06d0e0e58e99f7ae7fe0dbb3287cde&systemversion=8.2&device=iPhone7,2&broken=0&start=0&num=96&area=北京&secret=8e8475c7ad"
 **/
 //首页数据
